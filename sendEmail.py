982e24339111e774a37abe82fe4b50ce57aa79c0
felkerk <felkerk@gvsu.edu>
2024-03-07 10:43:01 -0500

    modifying sendmail to force explicit filename for attachments

--- sendEmail.py
+++ sendEmail.py
@@ -3,6 +3,8 @@ from email.message import EmailMessage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from config import emailFrom
+from email.mime.base import MIMEBase
+from email import encoders
 
 def sendEmail(emailTo, fromAddr, messageBody, subject):
   
@@ -23,13 +25,18 @@ def sendEmail(emailTo, fromAddr, messageBody, subject):
 
 def sendEmailWithAttachment(emailTo, fromAddr, subject, attachment):
   print("Attempting to Send Email report to " + emailTo)
+  filename = "report.csv"
   try:
-    emailBody = attachment
     message = MIMEMultipart()
     message['From'] = fromAddr
     message['To'] = emailTo
     message['Subject'] = subject   #The subject line
-    message.attach(MIMEText(emailBody, 'csv'))
+
+    part = MIMEBase('application', "octet-stream")
+    part.set_payload(attachment)
+    encoders.encode_base64(part)
+    part.add_header("Content-Disposition", "attachment; filename=%s" % filename)
+    message.attach(part)
     #Create SMTP session for sending the mail
     session = smtplib.SMTP('smtp.gvsu.edu', 25) #use gmail with port
     text = message.as_string()
